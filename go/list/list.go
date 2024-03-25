a5c5d5bc8e0401ee478ac3566b119caeac11ce10
Felipe Contreras <felipe.contreras@gmail.com>
2024-03-25 10:17:53 -0600

    go: first try

---
+++ go/list/list.go
@@ -0,0 +1,48 @@
+package llist
+
+import "unsafe"
+
+/* Circular doubly linked list implementation based on the Linux one */
+
+type Head[T any] struct {
+    next, prev *Head[T];
+}
+
+type Node[T any] struct {
+    head Head[T];
+    Data T;
+}
+
+func (head *Head[T]) Init() {
+    head.next = head;
+    head.prev = head;
+}
+
+func (head *Head[T]) IsEmpty() bool {
+    return head.next == head;
+}
+
+func (head *Head[T]) Add(n *Node[T]) {
+    h := &n.head;
+
+    head.next.prev = h;
+    h.next = head.next;
+    h.prev = head;
+    head.next = h;
+}
+
+func (head *Head[T]) Del(n *Node[T]) {
+    h := &n.head;
+
+    h.prev.next = h.next;
+    h.next.prev = h.prev;
+}
+
+func (head *Head[T]) Each() func(yield func(*T)) {
+    return func(yield func(*T)) {
+        // Pretend the *Head is a *Node
+        for p := (*Node[T])(unsafe.Pointer(head.next)); &p.head != head; p = (*Node[T])(unsafe.Pointer(p.head.next)) {
+            yield(&p.Data);
+        }
+    }
+}
